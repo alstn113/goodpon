95d728c3bcf3d3e50b2cedda771f3ac97889cb7f
alstn113 <alstn113@gmail.com>
2025-05-26 15:27:24 +0900

    initial commit

---
+++ src/test/kotlin/io/github/alstn113/payments/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.alstn113.payments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
