2d234c5d3bc15b99aaa36e33bb80a49ea43595ac
alstn113 <alstn113@gmail.com>
2025-06-09 17:44:59 +0900

    refactor: auth controller 응답 형식 변경

--- src/main/kotlin/io/github/alstn113/goodpon/api/dashboard/v1/AuthController.kt
+++ src/main/kotlin/io/github/alstn113/goodpon/api/dashboard/v1/AuthController.kt
@@ -1,9 +1,11 @@
 package io.github.alstn113.goodpon.api.dashboard.v1
 
+import io.github.alstn113.goodpon.api.dashboard.response.ApiResponse
 import io.github.alstn113.goodpon.application.auth.AuthFacadeService
 import io.github.alstn113.goodpon.application.auth.request.RegisterRequest
 import io.github.alstn113.goodpon.application.auth.request.ResendVerificationEmailRequest
 import io.github.alstn113.goodpon.application.auth.request.VerifyEmailRequest
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,17 +15,29 @@ class AuthController(
 ) {
 
     @PostMapping("/register")
-    fun registerAccount(@RequestBody request: RegisterRequest) {
+    fun registerAccount(
+        @RequestBody request: RegisterRequest,
+    ): ResponseEntity<ApiResponse<Unit>> {
         authFacadeService.registerAccount(request)
+
+        return ResponseEntity.ok(ApiResponse.success())
     }
 
     @GetMapping("/verify")
-    fun verifyEmail(@RequestBody request: VerifyEmailRequest) {
+    fun verifyEmail(
+        @RequestBody request: VerifyEmailRequest,
+    ): ResponseEntity<ApiResponse<Unit>> {
         authFacadeService.verifyEmail(request)
+
+        return ResponseEntity.ok(ApiResponse.success())
     }
 
     @PostMapping("/verify/resend")
-    fun resendVerificationEmail(@RequestBody request: ResendVerificationEmailRequest) {
+    fun resendVerificationEmail(
+        @RequestBody request: ResendVerificationEmailRequest,
+    ): ResponseEntity<ApiResponse<Unit>> {
         authFacadeService.resendVerificationEmail(request)
+
+        return ResponseEntity.ok(ApiResponse.success())
     }
 }
