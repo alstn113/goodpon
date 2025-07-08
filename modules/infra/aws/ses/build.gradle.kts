b8cda345b1fc234e2e70a617c9c89a40237c786f
alstn113 <alstn113@gmail.com>
2025-07-08 11:44:31 +0900

    refactpr: 인프라 설정 파일 api로 각각 이동

--- modules/infra/aws/ses/build.gradle.kts
+++ modules/infra/aws/ses/build.gradle.kts
@@ -1,8 +1,9 @@
 dependencies {
-    implementation(project(":modules:domain"))
     implementation(project(":modules:application:dashboard-application"))
 
     implementation(platform(libs.aws.sdk.bom))
     implementation("software.amazon.awssdk:sesv2")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
