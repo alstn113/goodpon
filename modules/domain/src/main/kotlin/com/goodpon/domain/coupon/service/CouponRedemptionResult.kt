b795353831b795003c2c029868015fec543f9b0b
alstn113 <alstn113@gmail.com>
2025-07-09 09:39:49 +0900

    chore: import 문 개선

--- modules/domain/src/main/kotlin/com/goodpon/domain/coupon/service/CouponRedemptionResult.kt
+++ modules/domain/src/main/kotlin/com/goodpon/domain/coupon/service/CouponRedemptionResult.kt
@@ -1,7 +1,9 @@
 package com.goodpon.domain.coupon.service
 
+import com.goodpon.domain.coupon.user.UserCoupon
+
 data class CouponRedemptionResult(
-    val redeemedCoupon: com.goodpon.domain.coupon.user.UserCoupon,
+    val redeemedCoupon: UserCoupon,
     val originalPrice: Int,
     val discountAmount: Int,
     val finalPrice: Int,
