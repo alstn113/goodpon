8436e9c05c0696a5608ac952e8d1bf37333f0e3d
alstn113 <alstn113@gmail.com>
2025-07-20 14:08:57 +0900

    feat: prometheus, grafana 설정 작성

--- modules/support/logging/build.gradle.kts
+++ modules/support/logging/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     api("io.micrometer:micrometer-tracing-bridge-brave")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 }
