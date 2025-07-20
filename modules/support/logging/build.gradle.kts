2e3b8c9cacb7aa2b5757843c5acfab9df61a9259
alstn113 <alstn113@gmail.com>
2025-07-20 14:43:10 +0900

    refactor: brave -> otlp로 변경

--- modules/support/logging/build.gradle.kts
+++ modules/support/logging/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    api("io.micrometer:micrometer-tracing-bridge-brave")
+    api("io.micrometer:micrometer-tracing-bridge-otel")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
 }
