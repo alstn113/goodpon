fe06a64600e630f9d70dc74a399baff18e324384
alstn113 <alstn113@gmail.com>
2025-07-23 11:47:20 +0900

    refactor: OTEL 의존성 제거

--- modules/support/logging/build.gradle.kts
+++ modules/support/logging/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     api("io.micrometer:micrometer-tracing-bridge-otel")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus")
 }
