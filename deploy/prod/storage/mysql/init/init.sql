2755e6f840a05f92318fa8db7af7cd8c4bf8c179
alstn113 <alstn113@gmail.com>
2025-07-28 21:52:19 +0900

    refactor: remove deploy prod

--- deploy/prod/storage/mysql/init/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'mysql-exporter-user'@'%' IDENTIFIED BY 'mysql-exporter-password' WITH MAX_USER_CONNECTIONS 3;
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'mysql-exporter-user'@'%';
-FLUSH PRIVILEGES;
