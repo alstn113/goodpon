bdf3f2226fc45693c9a579152a999e0f8dc8ee29
alstn113 <alstn113@gmail.com>
2025-07-30 09:26:21 +0900

    refactor: 테스트에 redis container, extensions 추가

--- modules/api/partner-openapi/build.gradle.kts
+++ modules/api/partner-openapi/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     runtimeOnly(project(":modules:infra:db:jpa"))
     runtimeOnly(project(":modules:infra:db:flyway"))
-    runtimeOnly(project(":modules:infra:db:redis"))
+    runtimeOnly(project(":modules:infra:redis"))
 
     implementation(project(":modules:domain"))
     implementation(project(":modules:application:partner-application"))
@@ -16,6 +16,7 @@ dependencies {
 
     testImplementation(project(":modules:infra:db:jpa"))
     testImplementation(project(":modules:infra:db:flyway"))
+    testImplementation(project(":modules:infra:redis"))
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation(libs.epage.restdocs.mockmvc)
@@ -23,6 +24,7 @@ dependencies {
     // 생성 후 조회 로직이 없는 것들을 테스트하기 위해 의존함. 코드 레벨에서 의존성을 분리했음.
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation(testFixtures(project(":modules:infra:db:jpa")))
+    testImplementation(testFixtures(project(":modules:infra:redis")))
 }
 
 openapi3 {
